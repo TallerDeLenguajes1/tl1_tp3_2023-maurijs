8e50854624fa7eee9f720909cd117a91175eed76
Mauricio <maurijo.suarez@gmail.com>
2023-04-08 01:44:11 -0300

    Ejercicio 2 terminado

--- ejer2.c
+++ ejer2.c
@@ -1,13 +1,21 @@
 #include <stdio.h>
 #include <stdlib.h>
 #include <time.h>
+#include <unistd.h>
+
 #define MES 12
 #define ANIO 5
 
 int main() 
 {
+    int minFecha[2];
+    int maxFecha[2];
+    int minimo, maximo;
     int produccion[ANIO][MES];
+    float ganancias[] = {0, 0, 0, 0, 0};
+
     srand(time(NULL));
+    sleep(1);
     for (int i = 0; i < ANIO; i++)
     {
         for (int j = 0; j < MES; j++)
@@ -17,9 +25,9 @@ int main()
         }
         printf("\n\n");
     }
+    sleep(1);
 
-    printf("\nCalculo de ganancias por anio\n\n");
-    float ganancias[] = {0, 0, 0, 0, 0};
+    printf("\nCalculo del promedio de ganancias por anio\n\n");
     for (int i = 0; i < ANIO; i++)
     {
         for (int j = 0; j < MES; j++)
@@ -28,10 +36,36 @@ int main()
         }
         ganancias[i] = ganancias[i]/12;
     }
-    printf("\nMuestra de ganancias por anio\n");
-    for (size_t i = 0; i < count; i++)
+    sleep(1);
+    printf("\nPromedio de ganancias por anio es:\n\n");
+    printf("\n2024 : %.2f\n", ganancias[0]);
+    printf("\n2025 : %.2f\n", ganancias[1]);
+    printf("\n2026 : %.2f\n", ganancias[2]);
+    printf("\n2027 : %.2f\n", ganancias[3]);
+    printf("\n2028 : %.2f\n", ganancias[4]);
+    sleep(1);
+    maximo = minimo = produccion[0][0];
+
+    printf("\nValor minimo y maximo anio\n");
+    sleep(1);
+    for (int i = 0; i < ANIO; i++)
     {
-        /* code */
+        for (int j = 0; j < MES; j++)
+        {
+            if (produccion[i][j] > maximo)  {
+                maximo = produccion[i][j];
+                maxFecha[0] = i; // ANIO
+                maxFecha[1] = j; // MES
+            } 
+            else if (produccion[i][j] < minimo)  {
+                minimo = produccion[i][j];
+                minFecha[0] = i;
+                minFecha[1] = j;
+            }       
+        }
     }
-    
+    printf("\nLa ganancia minima fue de %d y se obtuvo el %d/%d\n", minimo, minFecha[1], minFecha[0]+2024);
+    printf("\nLa ganancia maxima fue de %d y se obtuvo el %d/%d\n", maximo, maxFecha[1], maxFecha[0]+2024);
+
+    return 0;
 }
